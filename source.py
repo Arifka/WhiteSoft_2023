eadd74f989bbdd813d397134cbe2f40225349de9
Arifka <arif.99.99.arif@gmail.com>
2023-10-10 21:03:58 +1000

    adding solver module

--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 import json
 import requests 
 import output
+import solver
 
 url_data = "https://raw.githubusercontent.com/thewhitesoft/student-2023-assignment/main/data.json"
 url_replace = "https://raw.githubusercontent.com/thewhitesoft/student-2023-assignment/main/replacement.json"
@@ -11,18 +12,5 @@ data = response.json()
 response = requests.get(url_replace)
 replaceItems = response.json()
 
-result = []
-sizeRepItems = len(replaceItems)
-
-for str in data:
-    for indPair in range(sizeRepItems):
-        repEl = replaceItems[sizeRepItems - 1 - indPair]["replacement"]
-        sourceEl = replaceItems[sizeRepItems - 1 - indPair]["source"]
-        if (repEl in str):
-            if (sourceEl is None):
-                str = str.replace(repEl, "")
-            else:
-                str = str.replace(repEl, sourceEl)
-    result.append(str)
-
+result = solver(replaceItems, data)
 output.write_data2json(result)
