a413be2491a64e40497547a6d574fe88adfce0e9
Arifka <arif.99.99.arif@gmail.com>
2023-10-10 22:44:37 +1000

    minor changes

--- source.py
+++ source.py
@@ -2,11 +2,17 @@ import parse
 import output
 import solver
 
-url_data = "https://raw.githubusercontent.com/thewhitesoft/student-2023-assignment/main/data.json"
-url_replace = "https://raw.githubusercontent.com/thewhitesoft/student-2023-assignment/main/replacement.json"
+def main() -> int:
+    url_data = "https://raw.githubusercontent.com/thewhitesoft/student-2023-assignment/main/data.json"
+    url_replace = "https://raw.githubusercontent.com/thewhitesoft/student-2023-assignment/main/replacement.json"
 
-data = parse.get_data(url_data)
-replaceItems = parse.get_data(url_replace)
+    data = parse.get_data(url_data)
+    replaceItems = parse.get_data(url_replace)
 
-result = solver.replace_data(replaceItems, data)
-output.write_data2json(result)
+    result = solver.replace_data(replaceItems, data)
+    
+    output.write_data2json(result)
+    return 0
+
+if __name__ == '__main__':
+    main()
